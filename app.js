9d25578f4bb8d1fd4f13e3bf5b6701fe389574fc
kboeh <kboeh3@gmail.com>
2021-09-24 15:59:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelectorAll(".hamB, .nav-links li");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+const email = document.querySelectorAll(".fa-envelope, .fa-times-circle");
+const eContainer = document.querySelector(".email-container");
+
+// toggle for ham button and fade effect for links within drop-down menu
+// activates the listener when a link is clicked within the dropdown menu
+hamburger.forEach((hamburger) => {
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("open");
+        links.forEach(link => {
+            link.classList.toggle("fade");
+        });
+    });
+});
+
+//toggle for the envelope-icon and the x-icon (to open/close the email pop-up)
+email.forEach((email) => {
+    email.addEventListener("click", () => {
+        eContainer.classList.toggle("eToggle");
+
+    })
+})
