91ab2bf80ddf870ab8d5f6d65cdfcf40f1f587b4
KBoeh <kboeh3@gmail.com>
2022-05-04 16:29:02 -0400

    redesign

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const hamburger = document.querySelectorAll(".hamB, .nav-links li");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+// const z = document.querySelector(".project-container");
 
-const email = document.querySelectorAll(".fa-envelope, .fa-times-circle");
+const email = document.querySelectorAll(".email-button, .email-container > img, .overlay");
 const eContainer = document.querySelector(".email-container");
+const overlay = document.querySelector(".overlay");
 
 // toggle for ham button and fade effect for links within drop-down menu
 // activates the listener when a link is clicked within the dropdown menu
 hamburger.forEach((hamburger) => {
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("open");
+        // z.classList.toggle("open");
         links.forEach(link => {
             link.classList.toggle("fade");
         });
@@ -20,6 +23,7 @@ hamburger.forEach((hamburger) => {
 email.forEach((email) => {
     email.addEventListener("click", () => {
         eContainer.classList.toggle("eToggle");
+        overlay.classList.toggle("eToggle");
 
     })
 })
